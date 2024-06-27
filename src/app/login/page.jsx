176b5ddb830505464641b15873066556505575be
sheetalsinghfabcode <sheetal@thefabcode.org>
2024-06-27 12:56:31 +0530

    working on responsive

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -25,33 +25,35 @@ const Login = () => {
     const [isPasswordShow, setIsPasswordShow] = useState(false)
 
     return (
-        <div className="min-h-screen flex flex-col md:flex-row w-full">
+        <div className="min-h-screen flex flex-col relative md:flex-row w-full">
+            <div className="bg-[url('/image/login-bg.jpg')] h-screen bg-cover bg-center " >
+
             <div className="w-full relative hidden md:block max-w-[1166px]">
                 <ImageWrapper
-                    className="h-full"
+                    className="h-screen"
                     image={loginbg}
                     alt="Picture of the login background"
                 />
                 <LinkComponent
                     to="/"
                     withIcon={true}
-                    children={<><IconArrowBack className="fill-white " />Back to dashboard</>}
+                    children={<><IconArrowBack className="fill-white" />Back to dashboard</>}
                     className="absolute cursor-pointer flex items-center gap-x-2 text-white top-[40px] left-[40px]"
                 >
                 </LinkComponent>
                 <Text className="text-[15px] absolute left-[40px] bottom-[40px] leading-[22.5px] text-white font-semibold">© 2024 Copyright WorldWatch Market</Text>
             </div>
-            <div className="w-full  bg-white lg:w-1/2 flex items-start  md:items-center justify-center">
+            <div className="w-full absolute top-0  bg-white lg:w-1/2 flex items-start  md:items-center justify-center">
                 <div className="max-w-md w-full py-[18px] px-[20px]">
                     <Wrapper
-                    className="flex flex-col justify-center items-center gap-y-[12px]"
+                        className="flex flex-col justify-center items-center gap-y-[12px]"
                     >
                         <IconWarranty
-                        className="w-[41.25px] h-[34.98px]"
+                            className="w-[41.25px] h-[34.98px]"
                         />
-                    <H4 
-                    as="tag"
-                    className="font-semibold mb-10 text-center">Log in to WW/M to continue </H4>
+                        <H4
+                            as="tag"
+                            className="font-semibold mb-10 text-center">Log in to WW/M to continue </H4>
                     </Wrapper>
                     <form action="#" method="POST" className="space-y-3">
                         <div>
@@ -86,7 +88,7 @@ const Login = () => {
                                     className="absolute cursor-pointer right-[10px] top-[17px]"
                                 />}
                         </div>
-                        <Text className="text-left text-[#000] cursor-pointer leading-[24px]">Forget Password?</Text>
+                        <Text className="text-left text-[#000] text-[14px] cursor-pointer leading-[24px]">Forget Password?</Text>
                         <div>
                             <Button
                                 type="type"
@@ -100,9 +102,9 @@ const Login = () => {
                         <Text className="text-[#8C8C8C]">Don't have an account? </Text>
                         <Text className="text-black" >Sign up</Text>
                     </div>
-                    <div className="my-[40px] flex flex-row items-center justify-center text-center">
+                    <div className="my-[40px] flex flex-row gap-[18px]  items-center justify-center text-center">
                         <div className="h-[1.5px] bg-[#DCDAD8]  w-full max-w-[183px]"></div>
-                        <Text className="px-4 text-[#808080] font-semibold">OR</Text>
+                        <Text className="px-4 text-[#808080] text-[15px] leading-[22.5px] font-semibold">OR</Text>
                         <div className="h-[1.5px] bg-[#DCDAD8] w-full max-w-[183px]"></div>
                     </div>
                     <div className="mt-4 flex flex-col gap-y-3  items-center justify-between">
@@ -119,6 +121,8 @@ const Login = () => {
                     </div>
                 </div>
             </div>
+            </div>
+
         </div>
     )
 }
