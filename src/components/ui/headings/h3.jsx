982b222397fb23e19c1bf036cec0a3bff51c90f8
sheetalsinghfabcode <sheetal@thefabcode.org>
2024-06-27 18:56:43 +0530

    working on collection page

--- src/components/ui/headings/h3.jsx
+++ src/components/ui/headings/h3.jsx
@@ -2,27 +2,27 @@
 const H3 = ({ as, children, className }) => {
   return (
     <>
-    {as === "tag" && (
-      <h3
-        className={
-          (className || "") +
-          " text-[20px] font-semibold leading-[26.44px] tracking-[-0.01em]"
-        }
-      >
-        {children}
-      </h3>
-    )}
-    {as !== "tag" && (
-      <div
-        className={
-          (className || "") +
-          " text-[20px] font-semibold leading-[26.44px] tracking-[-0.01em]"
-        }
-      >
-        {children}
-      </div>
-    )}
-  </>
+      {as === "tag" && (
+        <h3
+          className={
+            (className || "") +
+            " text-[19px] font-medium leading-[25.88px] tracking-[-0.01em]"
+          }
+        >
+          {children}
+        </h3>
+      )}
+      {as !== "tag" && (
+        <div
+          className={
+            (className || "") +
+            " text-[20px] font-semibold leading-[26.44px] tracking-[-0.01em]"
+          }
+        >
+          {children}
+        </div>
+      )}
+    </>
   )
 }
 
